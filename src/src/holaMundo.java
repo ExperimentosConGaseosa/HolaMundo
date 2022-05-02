c36105418d7159fc7a186820df0d69d3e4ed6a66
RobertoGit <rgarcia.roberto@gmail.com>
2022-05-02 21:26:20 +0200

    incluye llamada a metodo para saludo

--- src/src/holaMundo.java
+++ src/src/holaMundo.java
@@ -10,6 +10,15 @@ public class holaMundo {
 		// comentario desde Linux 
 		
 	System.out.println("Hola Mundo!!");
+	
+	escribeMensaje(); 
+	
 	}
 
+	public static void escribeMensaje ()
+	{
+	System.out.println("\n\nDiciendo hola al mundo desde un método ... ");
+	}
+	
 }
+
